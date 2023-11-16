c71d42e39f734f0662d0bdedb7185ac6150efa64
Ernesto Storani Vidaurre <125527244+nestto@users.noreply.github.com>
2023-11-16 15:37:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-let userName = getFirstName('John-Doe', '-');
+// Função 
+const nomeHeroi = "Ernestinho";
+calculoSaldoRankeadas(0, 0);
 
-console.log(`Seja bem vindo ${userName}`);
+function calculoSaldoRankeadas(vitorias, derrotas) {
+  let resultadoRankeadas = vitorias - derrotas;
 
-function getFirstName(name, splitChar) {
-    let firstName = name.split(splitChar)[0];
-    return firstName;
+  if (vitorias <= 0) {
+    resultado = "Complete mais partidas para obter um resultado";
+  } else if (vitorias < 10) {
+    resultado = "Ferro";
+  } else if (vitorias >= 11 && vitorias <= 20) {
+    resultado = "Bronze";
+  } else if (vitorias >= 21 && vitorias <= 50) {
+    resultado = "Prata";
+  } else if (vitorias >= 51 && vitorias <= 80) {
+    resultado = "Ouro";
+  } else if (vitorias >= 81 && vitorias <= 90) {
+    resultado = "Diamante";
+  } else if (vitorias >= 91 && vitorias <= 100) {
+    resultado = "Lendário";
+  } else {
+    resultado = "Imortal";
+  }
+  return resultadoRankeadas;
 }
+
+// Exibindo mensagem de 
+console.log(`${nomeHeroi} está no ranking: ${resultado}`);
